2c0dc74671e5761b7883dbd9247f1e8ab770a879
Anankaaa <hegyaljai83@hotmail.com>
2022-08-31 17:01:36 +0200

    elso kesz

--- 09-storage/exercises/02/solution/js/cookieHandler.js
+++ 09-storage/exercises/02/solution/js/cookieHandler.js
@@ -1,1 +1,13 @@
+const cookieHandler = {
+  getAll() {
+    
+  }
+  toSessionStorage() {
+
+  }
+  flush(cookiesObject) {
+      document.cookie = `${cookiesObject}=; expires=Thu, 01 Jan 1970 00:00:00 UTC`
+  }
+}
+
 export default cookieHandler;
