f549b62506731780759b10af9ed82755da8eed75
Anankaaa <hegyaljai83@hotmail.com>
2022-08-31 19:42:55 +0200

    kész

--- 09-storage/exercises/02/solution/js/cookieHandler.js
+++ 09-storage/exercises/02/solution/js/cookieHandler.js
@@ -1,13 +1,25 @@
 const cookieHandler = {
   getAll() {
-    
-  }
+    let object = {};
+    const cookies = document.cookie.split('; ');
+    cookies.forEach((cookie) => {
+      const keyValueSplitted = cookie.split('=');
+      object[keyValueSplitted[0]] = keyValueSplitted[1];
+    });
+    return object;
+  },
   toSessionStorage() {
-
-  }
-  flush(cookiesObject) {
-      document.cookie = `${cookiesObject}=; expires=Thu, 01 Jan 1970 00:00:00 UTC`
-  }
-}
+    const object = this.getAll();
+    Object.entries(object).forEach(([key, value]) => {
+      sessionStorage.setItem(key, value);
+    });
+  },
+  flush() {
+    const object = this.getAll();
+    Object.keys(object).forEach((key) => {
+      document.cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+    });
+  },
+};
 
 export default cookieHandler;
